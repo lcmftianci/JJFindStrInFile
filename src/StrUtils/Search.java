e605ccf430406d72d7f7e3008560e9c41c23d89e
lcmftianci <707010543@qq.com>
2017-10-21 21:34:06 +0800

    比较字符串搜索与索引搜索的速度

--- src/StrUtils/Search.java
+++ src/StrUtils/Search.java
@@ -1,9 +1,14 @@
 package StrUtils;
 
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.util.Date;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+
+import javafx.scene.shape.Line;
 
 import org.apache.lucene.index.Term;
 import org.apache.lucene.index.TermDocs;
@@ -11,9 +16,11 @@ import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.TermQuery;
 
+import com.sun.javafx.collections.MappingChange.Map;
+
 public class Search {
 	
-	private String iNDEX_STORE_PATH = "E:\\Lucene\\index";
+	private String INDEX_STORE_PATH = "E:\\Lucene\\index";
 	
 	//利用Lucene搜索
 	public void indexSrearch(String seachType, String searchKey){
@@ -22,7 +29,7 @@ public class Search {
 			System.out.println("-----------------------------");
 			
 			//根据索引位置简历IndexSearcher
-			IndexSearcher searcher = new IndexSearcher(iNDEX_STORE_PATH);
+			IndexSearcher searcher = new IndexSearcher(INDEX_STORE_PATH);
 			//建立搜索单元
 			Term t = new Term(seachType, searchKey);
 			
@@ -58,6 +65,59 @@ public class Search {
 		
 		File filesDir = new File(searchDir);
 		
+		//返回目录文件夹文件数组
 		File[] files = filesDir.listFiles();
+		
+		//HashMap保存文件名和匹配次数对
+		LinkedHashMap rs = new LinkedHashMap();
+		
+		//记录搜索开始时间
+		Date beginTime = new Date();
+		
+		//遍历所有文件
+		for(int i = 0; i < files.length; ++i)
+		{
+			//初始化匹配次数
+			int hits = 0;
+			try {
+				//读取文件
+				BufferedReader br = new BufferedReader(new FileReader(files[i]));
+				StringBuffer sb = new StringBuffer();
+				String strLine = br.readLine();
+				while(strLine != null)
+				{
+					sb.append(strLine);
+					strLine = br.readLine();
+				}
+				br.close();
+				//将StringBuffer转化成String，便于搜索
+				String strToSearch = sb.toString();
+		
+				//初始化fromIndex
+				int fromIndex = -keyword.length();
+				
+				//逐个匹配关键字
+				while((fromIndex = strToSearch.indexOf(keyword, fromIndex + keyword.length())) != -1){
+					hits++;
+				}
+				//将文件名匹配次数加入到HashMap
+				rs.put(files[i].getName(), new Integer(hits));
+			} catch (Exception e) {
+				// TODO: handle exception
+				e.printStackTrace();
+			}
+		}
+		//输出查询结果
+		Iterator it = rs.keySet().iterator();
+		while (it.hasNext()) {
+			String fileName = (String)it.next();
+			Integer hits = (Integer)rs.get(fileName);
+			System.out.println("find " + hits.intValue() + "matches in " + fileName);
+		}
+		//记录结束时间
+		Date endTime = new Date();
+		//得到搜索耗费时间
+		long timeOfSearch = endTime.getTime() - beginTime.getTime();
+		System.out.println("使用字符串搜索总耗时 " + timeOfSearch + " ms");
 	}
 }
--- src/StrUtils/sampleMain.java
+++ src/StrUtils/sampleMain.java
@@ -16,11 +16,20 @@ public class sampleMain {
 			new File(outputDir).mkdir();
 			//new File(outputDir);
 		}
-		FindPreprocess.preprocess(new File(inputFile), outputDir);
-		System.out.print("Middle\n");
+		//FindPreprocess.preprocess(new File(inputFile), outputDir);
+		//System.out.print("Middle\n");
 		
-		IndexProcesser indexProcesser = new IndexProcesser();
-		indexProcesser.createIndex(outputDir);
-		System.out.print("End");
+		//IndexProcesser indexProcesser = new IndexProcesser();
+		//indexProcesser.createIndex(outputDir);
+		//System.out.print("End");
+		
+		Search search = new Search();
+		search.indexSrearch("content", "保尔");
+		
+		//插入一个分隔符
+		System.out.println();
+		
+		//通过字符串查找
+		search.stringSearch("保尔", outputDir);
 	}
 }
